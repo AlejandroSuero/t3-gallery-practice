5f3e455bf27a1111c2b117557824f1bc8de77cd8
AlejandroSuero <asuerome@gmail.com>
2024-04-18 20:24:45 +0200

    feat(page): UI differce when signedIn and signedOut

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,39 @@
 import { db } from "@/server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <section className="flex flex-wrap gap-4">
+      {images?.map((image) => (
+        <picture
+          key={image.id}
+          className="flex aspect-video w-48 flex-col items-center justify-between gap-2"
+        >
+          <img src={image.url} alt={image.name} />
+          <p>{image.name}</p>
+        </picture>
+      ))}
+    </section>
+  );
+}
+
+export default function HomePage() {
+  return (
     <main>
-      <section className="flex flex-wrap gap-4">
-        {images?.map((image) => (
-          <picture
-            key={image.id}
-            className="flex aspect-video w-48 flex-col items-center justify-between gap-2"
-          >
-            <img src={image.url} alt={image.name} />
-            <p>{image.name}</p>
-          </picture>
-        ))}
-      </section>
+      <SignedOut>
+        <div className="h-full w-full text-center text-4xl font-bold">
+          <p>Please sign in above to see images</p>
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
